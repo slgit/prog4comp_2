3c40001f0acfe233ef5b5b7c7eddc75da919121c
Svein Linge <sveinlin@simula.no>
2018-06-19 10:27:03 +0200

    updated naive_Newton.py with test block

--- py36-src/naive_Newton.py
+++ py36-src/naive_Newton.py
@@ -1,25 +1,16 @@
 def naive_Newton(f, dfdx, x, eps):
     while abs(f(x)) > eps:
-        x = x - (f(x))/dfdx(x)
+        x = x - float(f(x))/dfdx(x)
         print(x)
     return x
 
-def f(x):
-    return x**2 - 9
+def app_sqrt():
+    def f(x):
+        return x**2 - 9    
+    def dfdx(x):
+        return 2*x    
+    print(naive_Newton(f, dfdx, 1000, 0.001))
 
-def dfdx(x):
-    return 2*x
+if __name__ == '__main__':
+    app_sqrt()
 
-print(naive_Newton(f, dfdx, 1000, 0.001))
-
-# Another example...
-from math import tanh
-
-def g(x):
-    return tanh(x)
-
-def dgdx(x):
-    return 1 - tanh(x)**2
-
-#print(naive_Newton(f, dfdx, 1.08, 0.001))
-#print(naive_Newton(f, dfdx, 1.09, 0.001))  # change x_0 slightly
